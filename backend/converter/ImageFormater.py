8972d9879a4633f4b1e8c9aa8051f83d29d3c957
suntrackspb <spbwar@gmail.com>
2023-03-21 16:16:01 +0300

    move settings to Image Converter

--- backend/converter/ImageFormater.py
+++ backend/converter/ImageFormater.py
@@ -5,10 +5,33 @@ import cv2
 import os
 from collections import Counter
 from sklearn.cluster import KMeans
-from img_settings import landscape_settings, portrait_settings
 from dotenv import load_dotenv
 
 load_dotenv()
+
+portrait_settings = {
+    "x": 135,
+    "y": 400,
+    "ix": 265,
+    "iy": 400,
+    "sx": 10,
+    "sy": 0,
+    "ex": 135,
+    "ey": 72,
+    "axis": 1
+}
+
+landscape_settings = {
+    "x": 400,
+    "y": 135,
+    "ix": 400,
+    "iy": 265,
+    "sx": 0,
+    "sy": 10,
+    "ex": 72,
+    "ey": 135,
+    "axis": 0
+}
 
 
 def read_image():
