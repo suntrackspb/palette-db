7a8e60fd4044cc7b41586dce471d63d8a5a82984
Danil Nepomnyashchiy <idioma@mail.ru>
2023-03-31 21:08:15 +0300

    refactor hook

--- frontend/src/hooks/useConfirmPassword.js
+++ frontend/src/hooks/useConfirmPassword.js
@@ -3,23 +3,21 @@ import {useEffect, useState} from "react";
 const useConfirmPassword = (initialValue, password) => {
   const [value, setValue] = useState(initialValue);
   const [error, setError] = useState('');
+
   const onChange = e => {
     const value = e.target.value
     setValue(value)
-    if (value !== password) {
-      setError('Пароли не совпадают')
-    }
-    else {
-      setError('')
-    }
+    toggleError(value)
   }
+  
+  const toggleError = value => {
+    value && value !== password
+      ? setError('Пароли не совпадают')
+      : setError('')
+  }
+
   useEffect(() => {
-    if (!!value && value !== password) {
-      setError('Пароли не совпадают')
-    }
-    else {
-      setError('')
-    }
+    toggleError(value)
   }, [password]);
 
   return {value, setValue, error, onChange}
