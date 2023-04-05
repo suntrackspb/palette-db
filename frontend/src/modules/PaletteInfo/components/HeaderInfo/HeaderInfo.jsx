64fce6e857f16221060e46c8daab553ed083029d
Danil Nepomnyashchiy <idioma@mail.ru>
2023-04-05 15:09:52 +0300

    hide email on palette owner

--- frontend/src/modules/PaletteInfo/components/HeaderInfo/HeaderInfo.jsx
+++ frontend/src/modules/PaletteInfo/components/HeaderInfo/HeaderInfo.jsx
@@ -7,12 +7,13 @@ import useAuth from "../../../../hooks/useAuth.js";
 const HeaderInfo = ({data}) => {
   const {date, owner, name, _id} = data
   const {store} = useAuth()
+  const shortOwner = owner.split('@').at(0)
 
   return (
     <>
       <PageTitle title={name} m={0}/>
       <Typography>Дата создания: {new Date(date).toLocaleDateString()}</Typography>
-      <Typography>Добавил: {owner === 'host' ? 'Palette Picker' : owner}</Typography>
+      <Typography>Добавил: {owner === 'host' ? 'Palette Picker' : shortOwner}</Typography>
       {store.isAuth &&
         <Box sx={{
           position: 'absolute',
