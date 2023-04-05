254ecff0d0a6685cd601ccbbb75e840cc40e8681
Danil Nepomnyashchiy <idioma@mail.ru>
2023-04-05 15:25:28 +0300

    rework header

---
+++ frontend/src/components/Header/styles.js
@@ -0,0 +1,20 @@
+export const styles = {
+  appBar: {
+    padding: 2,
+    bgcolor: 'header.main',
+    backgroundImage: 'none',
+    position: 'relative',
+    zIndex: 2
+  },
+  container: {
+    display: 'flex',
+    gap: '10px',
+    alignItems: 'center'
+  },
+  rightBlock: {
+    marginLeft: 'auto',
+    display: 'flex',
+    alignItems: 'center',
+    gap: '10px'
+  }
+}
