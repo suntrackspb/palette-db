bcabb292a5a4e726abea24354c42167ddb1ff8d6
Danil Nepomnyashchiy <idioma@mail.ru>
2023-04-06 17:04:51 +0300

    rework ui input

--- frontend/src/components/UI/Inputs/Input.jsx
+++ frontend/src/components/UI/Inputs/Input.jsx
@@ -1,17 +1,31 @@
-import {FormControl, InputLabel, OutlinedInput} from "@mui/material";
+import {FormControl, FormHelperText, InputLabel, OutlinedInput} from "@mui/material";
 
-const Input = ({value, onChange, label, id, type, required}) => {
+const Input = ({
+  value,
+  onChange,
+  label,
+  id,
+  type,
+  required,
+  autoFocus,
+  error,
+  errorMessage
+}) => {
   return (
-    <FormControl variant='outlined' sx={{width: '100%'}} required={required}>
+    <FormControl sx={{width: '100%'}} required={required}>
       <InputLabel htmlFor={id}>{label}</InputLabel>
       <OutlinedInput
         value={value}
         onChange={onChange}
+        autoFocus={autoFocus}
         id={id}
         label={label}
         type={type}
-        autoComplete='off'
+        error={error}
       />
+      <FormHelperText id={id} sx={{color: '#f44336'}}>
+        {errorMessage}
+      </FormHelperText>
     </FormControl>
   );
 };
