f1de260c8e5867501cc01586f424946c2bd24dad
Danil Nepomnyashchiy <idioma@mail.ru>
2023-04-06 22:04:04 +0300

    rework useCaptcha, useeffect for captcha visible added

--- frontend/src/hooks/useCaptcha.js
+++ frontend/src/hooks/useCaptcha.js
@@ -1,12 +1,18 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
-const useCaptcha = () => {
+const useCaptcha = (depsArr) => {
   const [isCaptchaDone, setIsCaptchaDone] = useState(false);
+  const [isCaptchaVisible, setIsCaptchaVisible] = useState(false);
+
+  useEffect(() => {
+    depsArr.every(dep => dep !== '') && setIsCaptchaVisible(true)
+  }, depsArr);
+
   const handleCaptcha = value => {
     value
       ? setIsCaptchaDone(true)
       : setIsCaptchaDone(false)
   }
-  return {isCaptchaDone, handleCaptcha}
+  return {isCaptchaDone, isCaptchaVisible, setIsCaptchaVisible, handleCaptcha}
 }
 export default useCaptcha
