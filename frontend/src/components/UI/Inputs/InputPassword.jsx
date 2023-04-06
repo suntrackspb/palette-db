57f24a142abf146a84a07b7cd3c18e291559f34d
Danil Nepomnyashchiy <idioma@mail.ru>
2023-04-06 17:04:38 +0300

    add ui input password

---
+++ frontend/src/components/UI/Inputs/InputPassword.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import {FormControl, FormHelperText, IconButton, InputAdornment, InputLabel, OutlinedInput} from "@mui/material";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
+
+const InputPassword = ({
+  value,
+  onChange,
+  label,
+  id,
+  required,
+  autoFocus,
+  error,
+  errorMessage
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword(prev => !prev);
+  return (
+    <FormControl variant="outlined" required={required}>
+      <InputLabel htmlFor={id}>{label}</InputLabel>
+      <OutlinedInput
+        value={value}
+        onChange={onChange}
+        id={id}
+        type={showPassword ? 'text' : 'password'}
+        label={label}
+        error={error}
+        autoFocus={autoFocus}
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="Показать/скрыть пароль"
+              onClick={handleClickShowPassword}
+              edge="end"
+            >
+              {showPassword ? <VisibilityOff/> : <Visibility/>}
+            </IconButton>
+          </InputAdornment>}
+      />
+      <FormHelperText id="outlined-password" sx={{color: '#f44336'}}>
+        {errorMessage}
+      </FormHelperText>
+    </FormControl>
+  );
+};
+
+export default InputPassword;
